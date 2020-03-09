45a16d00bc1f50d212f96b4bfb452f54b4366b54
James Benjamin <james.abenj@gmail.com>
2020-03-09 17:25:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "my name is James"
+  end
+
+  get "/hometown" do
+    "my hometown is East Hartford, CT"
+  end
+
+  get "/favorite-song"
+    "my favorite song is Waiting in Vain by Bob Marley"
+
 end
